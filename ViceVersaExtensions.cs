5ce897123b086b6ae1cb9b0223ff69def86a029d
Librazy <im@librazy.org>
2017-12-19 21:52:04 +0800

    init

---
+++ ViceVersaExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Xmu.Crms.Services.ViceVersa;
+using Xmu.Crms.Shared.Service;
+
+namespace Microsoft.Extensions.DependencyInjection
+{ 
+    public static class ViceVersaExtensions
+    {
+        // 为每一个你写的Service写一个这样的函数，把 UserService 替换为你实现的 Service
+        public static IServiceCollection AddViceVersaUserService(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection.AddScoped<IUserService, UserService>();
+        }
+    }
+}
