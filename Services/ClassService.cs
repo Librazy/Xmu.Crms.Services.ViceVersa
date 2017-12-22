b5e7cd10d0a4354c1f945092a9c0b3056eba9c03
luoxudan <luoxudan@users.noreply.github.com>
2017-12-22 17:18:38 +0800

    update

--- Services/ClassService.cs
+++ Services/ClassService.cs
@@ -10,6 +10,7 @@ namespace Xmu.Crms.Services.ViceVersa
     class ClassService : IClassService
     {
         //private readonly ISeminarService _seminarService;
+        //private readonly ICourseService _courseService;
 
         private readonly IClassDao _classDao;
         public  ClassService(IClassDao classDao)
@@ -17,22 +18,21 @@ namespace Xmu.Crms.Services.ViceVersa
             _classDao = classDao;
             //_seminarService = seminarService;
         }
-        
 
-        public bool DeleteClassByClassId(long classId)
+        public void DeleteClassByClassId(long classId)
         {
-            try
-            {
-                _classDao.Delete(classId);
-                return true;
-            }
-            catch(ClassNotFoundException ec)
-            {
-                throw;
-            }
+            //    try
+            //    {
+            //        _classDao.Delete(classId);
+            //        return true;
+            //    }
+            //    catch(ClassNotFoundException ec)
+            //    {
+            //        throw ;
+            //    }
         }
 
-        public bool DeleteClassByCourseId(long courseId)
+        public void DeleteClassByCourseId(long courseId)
         {
             throw new NotImplementedException();
         }
@@ -42,12 +42,12 @@ namespace Xmu.Crms.Services.ViceVersa
             throw new NotImplementedException();
         }
 
-        public bool DeleteCourseSelectionById(long userId, long classId)
+        public void DeleteCourseSelectionById(long userId, long classId)
         {
             throw new NotImplementedException();
         }
 
-        public bool DeleteScoreRuleById(long classId)
+        public void DeleteScoreRuleById(long classId)
         {
             throw new NotImplementedException();
         }
@@ -57,6 +57,7 @@ namespace Xmu.Crms.Services.ViceVersa
             throw new NotImplementedException();
         }
 
+        /// 按班级id获取班级详情.
         public ClassInfo GetClassByClassId(long classId)
         {
             try
@@ -70,13 +71,7 @@ namespace Xmu.Crms.Services.ViceVersa
 
         public ClassInfo GetScoreRule(long classId)
         {
-            try
-            {
-                var classinfo = _classDao.Get(classId);
-
-                return classinfo;
-            }
-            catch { throw; }
+            throw new NotImplementedException();
         }
 
         public long InsertClassById(long userId, long courseId)
@@ -91,37 +86,83 @@ namespace Xmu.Crms.Services.ViceVersa
 
         public long InsertScoreRule(long classId, ClassInfo proportions)
         {
-            try
-            {
-                _classDao.Update(proportions);
-                return 1;    //???
-            }
-            catch { throw; }
+            throw new NotImplementedException();
         }
 
         public List<ClassInfo> ListClassByCourseId(long courseId)
         {
-            try
-            {
-                List<ClassInfo> list = _classDao.QueryAll(courseId);
-                return list;
-            }
-            catch { throw; }
+            throw new NotImplementedException();
         }
 
+        /// 按课程名称和教师名称获取班级列表.
         public List<ClassInfo> ListClassByName(string courseName, string teacherName)
         {
-            throw new NotImplementedException();
+            List<ClassInfo> classList = null;
+            if (courseName!=null)
+            {
+              //  List<Course>courseList= _courseService.ListCourseByCourseName(courseName);
+                List<ClassInfo> templist=null;
+               // if (courseList == null) return null;
+               // foreach (Course c in courseList)
+                {
+                    templist = _classDao.QueryAll(c.Id);
+                    classList.AddRange(templist);
+                }
+            }
+            else if(teacherName!=null)
+            {
+               // List<Course> courseList = _courseService.ListClassByTeacherName(teacherName);
+            }
+            return classList;
         }
 
-        public bool UpdateClassByClassId(long classId)
+        public void UpdateClassByClassId(long classId)
         {
             throw new NotImplementedException();
         }
 
-        public bool UpdateScoreRule(long classId, ClassInfo proportions)
+        public void UpdateScoreRule(long classId, ClassInfo proportions)
         {
             throw new NotImplementedException();
         }
+
+
+        //public bool DeleteClassByClassId(long classId)
+        //{
+       
+        //}
+
+
+
+        //public ClassInfo GetClassByClassId(long classId)
+        //{
+       
+        //}
+
+        //public ClassInfo GetScoreRule(long classId)
+        //{
+        //    try
+        //    {
+        //        var classinfo = _classDao.Get(classId);
+
+        //        return classinfo;
+        //    }
+        //    catch { throw; }
+        //}
+
+
+
+        //public List<ClassInfo> ListClassByCourseId(long courseId)
+        //{
+        //    try
+        //    {
+        //        List<ClassInfo> list = _classDao.QueryAll(courseId);
+        //        return list;
+        //    }
+        //    catch { throw; }
+        //}
+
+
+
     }
 }
